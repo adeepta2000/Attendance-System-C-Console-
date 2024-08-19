8af96e7c0b803eb428f26d93d3b4eea4260705eb
Adeepta Shushil <adeepto04@gmail.com>
2024-08-19 22:04:25 +0600

    Changes in DbContext

--- AttendanceSystem/AttendanceSystem/Model/ProjectDbContext.cs
+++ AttendanceSystem/AttendanceSystem/Model/ProjectDbContext.cs
@@ -14,9 +14,6 @@ namespace AttendanceSystem.Model
 
         public ProjectDbContext()
         {
-            //MyConnectionString
-            //_connectionString = "Server=AdeeptaShushil;Database=CSharpB17;User Id=sa;Password=12345;Trust Server Certificate=True";
-
             //ProjectConnectionString
             _connectionString ="Data Source=.\\SQLEXPRESS;Initial Catalog=CSharpB16;User ID=csharpb17; Password = 123456; TrustServerCertificate = True";
         }
